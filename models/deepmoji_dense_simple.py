cc386185b6ff4a2515f26a07ea0143fd9dac15d2
geekSiddharth <siddharth16268@iiitd.ac.in>
2018-09-01 23:43:27 +0530

    kcuh toh gadbad hai

--- models/deepmoji_dense_simple.py
+++ models/deepmoji_dense_simple.py
@@ -19,7 +19,7 @@ def get_model_1():
     return model
 
 
-def get_model_2()
+def get_model_2():
     """This is regularised"""
     inputs = Input(shape=(6912,))
     model = Dense(3951, activation='relu')(inputs)
